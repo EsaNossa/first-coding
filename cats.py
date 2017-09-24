adf1e91f262aaaa2f4525480cfe080431c736525
nossa <nossa@nossas-MacBook-Pro.local>
2017-09-24 22:36:02 +0200

    first functions with if and placeholder

---
+++ cats.py
@@ -0,0 +1,8 @@
+def name(women, cats):
+    if women < cats:
+    	return "Too many cats! The world is crazy!"
+    elif women > cats:
+    	return "Not many cats! The world is saved!"
+    else:
+    	return "You gave a wrong number"
+
---
+++ cv.py
@@ -0,0 +1,6 @@
+def cv(name, age, country, interest):
+	print "My name is %s" % name
+	print "I am %d old." % age
+	print "I come from %s." % country
+	print "I like %s." % interest
+    
